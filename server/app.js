5bb316589cd753f211fd83b0a83d934ab425eed9
Daniel Teel <daniel.teel.2@us.af.mil>
2022-01-29 13:32:49 -0700

    Added web-ui folder

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
+const express = require('express');
+const cors=require('cors');
 const net = require('net');
 
-let lastHoistCommand = 15;
-let lastPowerState = 0;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let lastHoistCommand=15;
+let lastPowerState=32;
+let lastJettisonState=34;
 
 let ue4Socket = null;
 let inputSocket = null;
@@ -21,6 +28,8 @@ const inputServer = net.createServer((socket) => {
                 lastHoistCommand=code;
             }else if (code===32 || code===33){
                 lastPowerState=code;
+            }else if (code==34 || code===35){
+                lastJettisonState=code;
             }
         }
         if (ue4Socket) ue4Socket.write(d);
@@ -44,9 +53,11 @@ const ue4Server = net.createServer((socket) => {
     socket.on('error', onError);
 
 
-    socket.write(Uint8Array.from([lastPowerState, lastHoistCommand]))
-    console.log(Uint8Array.from([lastPowerState, lastHoistCommand]));
+    socket.write(Uint8Array.from([lastPowerState, lastHoistCommand, lastJettisonState]))
+    console.log(Uint8Array.from([lastPowerState, lastHoistCommand, lastJettisonState]));
     function onData(d) {  
+        console.log('RADALT:',d[0]*2);
+        console.log('HOIST OUT:',d[1]);
     }
     function onClose() {  
         ue4Socket=null;
@@ -57,12 +68,19 @@ const ue4Server = net.createServer((socket) => {
     }  
 });
 
-
-inputServer.listen(3001, ()=>{
-    console.log('input server listening to ',inputServer.address());
+app.get('/reset', (req, res)=>{
+    if (ue4Socket){
+        ue4Socket.write(Uint8Array.from([255]));
+        return res.status(200).json({message: 'success'});
+    } else {
+        return res.status(400).json({error: 'Game isnt connected'});
+    }
 });
 
+app.use(express.static('public'));
 
-ue4Server.listen(3002, ()=>{
-    console.log('UE4 server listening to ',ue4Server.address());
-});
+app.listen(3000, () => console.log('express listening on 3000') );
+
+inputServer.listen(3001, () => console.log('input server listening to ',inputServer.address()) );
+
+ue4Server.listen(3002, () => console.log('UE4 server listening to ',ue4Server.address()) );
